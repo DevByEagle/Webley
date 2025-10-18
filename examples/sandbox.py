6aeae667c8fe846d337c5be34ccec8a4f996a09b
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-10-18 22:36:35 +0000

    updated all

--- examples/sandbox.py
+++ examples/sandbox.py
@@ -2,11 +2,8 @@ import webley as wb
 
 @wb.route("/")
 def home(request):
-    return wb.http.HttpResponse(b"<h1>Hello, World</h1>")
-
-@wb.route("/about")
-def about(request):
-    print(request.path)
-    return wb.http.HttpResponse(b"<h1>About</h1>")
+    return wb.http.HttpResponse(b"<h1>Hello, Webley!</h1>")
 
 wb.run()
+
+Exception
