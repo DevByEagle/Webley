f7d1f8b78dac6cd2f3b373f37e3eca9a45ba8762
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-10-20 03:20:07 +0000

    updated sandbox.py

--- examples/sandbox.py
+++ examples/sandbox.py
@@ -1,14 +1,1 @@
 import webley
-from webley.auth import User
-
-def login_user():
-    user = User("John", "john@exmaple.com")
-    user.login()
-
-@webley.route("/")
-def index(request):
-    button = f'<button type="submit" onClick={login_user}>login</button>'
-    content = button.encode()
-    return webley.http.HttpResponse(content)
-
-webley.run()
