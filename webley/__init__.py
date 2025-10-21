c8abaa5f8bcf08523fc25b39d5da45300281652d
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-10-21 18:45:03 -0400

    Refactor __init__.py to include core module

--- webley/__init__.py
+++ webley/__init__.py
@@ -1,25 +1,30 @@
-""" """
-
+"""
+"""
 import warnings
 
-__version__ = "1.1.0.dev0"
+from . import core
+from .core import (
+    route,
+    run
+)
 
 from . import http
 
-__all__ = list({"http"} | set(http.__all__) | {"__version__"})
-
+__all__ = list(
+    {"http"} | 
+    set(core.__all__) |
+    set(http.__all__) | 
+    {"__version__"}
+)
 
 def __getattr__(attr):
     if attr == "http":
         import webley.http as http
-
         return http
     raise AttributeError(f"module {__name__!r} has no attribute {attr!r}")
-
 
 def __dir__():
     pass
 
-
 # Clean up namespace and remove standard library and package references.
-del warnings
+del warnings, core
