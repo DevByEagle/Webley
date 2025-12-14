0b1159db6a957d5c99f75a22d0b6ea3a686cd221
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-12-14 19:18:28 +0000

    Refactor HttpResponse class: remove unused attributes and methods; delete obsolete router and utility files

--- webley/http/response.py
+++ webley/http/response.py
@@ -1,15 +1,11 @@
-from typing import Any, Dict
-
 class HttpResponse:
 
     status_code = 200
 
     def __init__(self, content=b"", *args, **kwargs):
-        self.headers: Dict[str, str] = {}
-        self.content = content
-        # default content-type
-        if isinstance(content, (str, bytes)):
-            self.headers.setdefault("Content-Type", "text/html")
+        pass
 
     def __repr__(self):
-        return ""
+        pass
+    
+    
